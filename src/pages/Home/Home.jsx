78c5474f01b0682735eb9c863e7710d04a6df052
andre-sqr <andrebsqr@gmail.com>
2022-08-25 18:04:51 -0300

    página Home iniciada

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import S from './Home.module.css'
+
+const Home = () => {
+  return (
+    <div className={S.main}>
+        <h1>TESTE</h1>
+    </div>
+  )
+}
+
+export default Home
