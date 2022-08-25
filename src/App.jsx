15a97496508b834fb74b64f49aad9edd8a1aa361
andre-sqr <andrebsqr@gmail.com>
2022-08-25 18:02:55 -0300

    adicionando rotas no app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import SideMenu from './components/SideMenu/SideMenu'
+import CustomersList from './pages/CustomersList/CustomersList'
+import Home from './pages/Home/Home'
+import Register from './pages/Register/Register'
+import S from './App.module.css'
 
 function App() {
   
   return (
-    <div className="App">
- 
+    <div className={S.wholePage}>
+   <BrowserRouter>
+    {/* static */}
+    <SideMenu/>
+    <Routes>
+    {/* dynamic */}
+      <Route path='/' element={<Home/>}/>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/customers' element={<CustomersList/>}/>
+    </Routes>
+   </BrowserRouter> 
     </div>
   )
 }
