f94d63bc152cd8ad0ce8a0ebe080e8f8fd1fb01b
andre-sqr <andrebsqr@gmail.com>
2022-08-25 18:03:07 -0300

    componente de botão criado

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = ({style, text, onClick}) => {
+  return (
+    <button className={style} onClick={onClick}>{text}</button>
+  )
+}
+
+export default Button
