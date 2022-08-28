a1eb61b53de93131813173eb9abb8806d1039d17
andre-sqr <andrebsqr@gmail.com>
2022-08-28 20:52:44 -0300

    criando card de clientes

---
+++ src/components/CustomerCard/CustomerCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const CustomerCard = () => {
+  return (
+    <div>
+        <h1>Nome + Sobrenome</h1>
+        <p>Email:</p>
+        <p>Telefone:</p>
+        <p>Endereço 1:</p>
+        <p>Endereço 2:</p>
+        <p>Data de nascimento:</p>
+        <p>CPF:</p>
+    </div>
+  )
+}
+
+export default CustomerCard
